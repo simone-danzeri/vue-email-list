45a0b1dbcfb87f9fdb6f98639c9782a40da3727a
simone-danzeri <s.danzeri@icloud.com>
2024-04-09 13:20:39 +0200

    feat: test iniziale per vedere che axios funzioni correttamente

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 /* Attraverso l'apposita API di Boolean
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista. */
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            mailArray: []
+        };
+    },
+    methods: {
+        getMailFromApi() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                console.log(response.data);
+            })
+        }
+    }
+}).mount('#app');
